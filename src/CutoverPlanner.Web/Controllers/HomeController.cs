bd2d349c0d29f659101b505f2dbeaa84f66313e6
Diego Anzolin Ferreira <diego.anzolin@energisa.com.br>
2026-03-12 17:42:17 -0300

    Ajustes

--- src/CutoverPlanner.Web/Controllers/HomeController.cs
+++ src/CutoverPlanner.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using CutoverPlanner.Domain.Enumerations;
 using CutoverPlanner.Web.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CutoverPlanner.Web.Controllers
 {
@@ -15,13 +16,29 @@ namespace CutoverPlanner.Web.Controllers
             var emAnd = _db.Atividades.Count(a => a.Status == StatusAtividade.EmAndamento);
             var naoIni = total - concl - emAnd;
             var riscos = _db.Atividades.Where(a => a.RiscoGoLive == true).OrderBy(a => a.Inicio).Take(10).ToList();
-            
+
+            // distribuição por área e por marco
+            var distArea = _db.Atividades
+                             .Include(a => a.Executor).ThenInclude(e => e.Area)
+                             .Where(a => a.Executor != null && a.Executor.Area != null)
+                             .GroupBy(a => a.Executor.Area!.Nome)
+                             .Select(g => new { Name = g.Key, Count = g.Count() })
+                             .ToList();
+            var distMarco = _db.Atividades
+                              .Include(a => a.Marco)
+                              .Where(a => a.Marco != null)
+                              .GroupBy(a => a.Marco!.Nome)
+                              .Select(g => new { Name = g.Key, Count = g.Count() })
+                              .ToList();
+
             ViewBag.Total = total;
             ViewBag.Concl = concl;
             ViewBag.EmAnd = emAnd;
             ViewBag.NaoIni = naoIni;
             ViewBag.Riscos = riscos;
             ViewBag.Pct = total>0 ? (int)(concl*100.0/total) : 0;
+            ViewBag.DistArea = distArea;
+            ViewBag.DistMarco = distMarco;
 
             return View();
         }
