55303566ec2b8d7d2eea5182dac32e760c4e5557
Diego Anzolin Ferreira <diego.anzolin@energisa.com.br>
2026-03-19 14:56:27 -0300

    Registros padrões

--- src/CutoverPlanner.Web/Data/AppDbContext.cs
+++ src/CutoverPlanner.Web/Data/AppDbContext.cs
@@ -14,6 +14,8 @@ namespace CutoverPlanner.Web.Data
         
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+            #region Executores
+
             var areas = new List<Area>
             {
                 new Area()
@@ -54,7 +56,7 @@ namespace CutoverPlanner.Web.Data
                 },
                 new Area()
                 {
-                    Nome = "GEAD/COBRANÇA",
+                    Nome = "GEAD/FATURAMENTO",
                     NomeResponsavel = "Eder Marcos da Silva",
                     EmailResponsavel = ""
                 },
@@ -67,7 +69,13 @@ namespace CutoverPlanner.Web.Data
                 new Area()
                 {
                     Nome = "INFRA APLICAÇÕES",
-                    NomeResponsavel = "Andrea Bittencourt",
+                    NomeResponsavel = "Andrea Cristina Bittencourt Moraes",
+                    EmailResponsavel = ""
+                },
+                new Area()
+                {
+                    Nome = "INFRA DATACENTER E CLOUD",
+                    NomeResponsavel = "Valeria Ferreira Victor",
                     EmailResponsavel = ""
                 },
                 new Area()
@@ -91,6 +99,193 @@ namespace CutoverPlanner.Web.Data
 
                 this.SaveChanges();
             }
+
+            #endregion
+
+            #region Executores
+
+            var executores = new List<Executor>
+            {
+                new Executor()
+                {
+                    Nome = "Genérico",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "Genérica").Id
+                },
+                new Executor()
+                {
+                    Nome = "Wagner Bianchini Narde",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO DESPACHO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Daniel Felipe Borba",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO DESPACHO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Douglas Torres Cravo Borges",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO DESPACHO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Diego Anzolin Ferreira",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO DESPACHO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Bruno Barroso Miranda",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO DESPACHO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Ana Cristina Barbosa Faria",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO DESPACHO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Victor Teixeira Pinheiro",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/CANAIS CONVENCIONAIS").Id
+                },
+                new Executor()
+                {
+                    Nome = "Valeria Martelloti da Silva",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/CANAIS CONVENCIONAIS").Id
+                },
+                new Executor()
+                {
+                    Nome = "Lucas de Almeida Teixeira",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/CANAIS CONVENCIONAIS").Id
+                },
+                new Executor()
+                {
+                    Nome = "Marco Aurelio Vilela Sousa",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/CANAIS CONVENCIONAIS").Id
+                },
+                new Executor()
+                {
+                    Nome = "Diogo de Souza Miranda",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/COBRANÇA").Id
+                },
+                new Executor()
+                {
+                    Nome = "Thiago Rodrigues e Rodrigues",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/COBRANÇA").Id
+                },
+                new Executor()
+                {
+                    Nome = "Marcus Vinicius Alves de Castro",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO MANUTENÇÃO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Otavio de Barros Freitas",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "GEAD/OPERAÇÃO MANUTENÇÃO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Andrea Cristina Bittencourt Moraes",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA APLICAÇÕES").Id
+                },
+                new Executor()
+                {
+                    Nome = "Joao Luiz de Souza Torres",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA APLICAÇÕES").Id
+                },
+                new Executor()
+                {
+                    Nome = "Ghullite Tacone Bento",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA APLICAÇÕES").Id
+                },
+                new Executor()
+                {
+                    Nome = "Valeria Ferreira Victor",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA DATACENTER E CLOUD").Id
+                },
+                new Executor()
+                {
+                    Nome = "Carlos Roberto Pereira de Aquino",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA DATACENTER E CLOUD").Id
+                },
+                new Executor()
+                {
+                    Nome = "Carlos Andre Souza",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA DATACENTER E CLOUD").Id
+                },
+                new Executor()
+                {
+                    Nome = "Rebeca Vitoria Costa Souza",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA DATACENTER E CLOUD").Id
+                },
+                new Executor()
+                {
+                    Nome = "Erica de Andrade e Santos",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "PROJETO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Bruno Miranda Couto",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "PROJETO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Jean Carlo Ranucci do Amaral",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "USUÁRIO").Id
+                },
+                new Executor()
+                {
+                    Nome = "Daniel Eduardo Garrido Barzellay",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA BANCO DE DADOS").Id
+                },
+                new Executor()
+                {
+                    Nome = "Andre Domingues dos Santos Lomba",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA BANCO DE DADOS").Id
+                }
+                ,
+                new Executor()
+                {
+                    Nome = "Eduardo Celso de Paula",
+                    Email = "",
+                    IdArea = this.Areas.First(q => q.Nome == "INFRA BANCO DE DADOS").Id
+                }
+            };
+
+            foreach (var executor in executores)
+            {
+                if (!this.Executores.Any(q => q.Nome.Equals(executor.Nome)))
+                    this.Add(executor);
+
+                this.SaveChanges();
+            }
+
+            #endregion
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
